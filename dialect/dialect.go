8baad246f846063e4d027ed3ea9c6e02e2b73105
yuxintao <yuxintao.jingtao8a@bytedance.com>
2025-04-21 17:43:10 +0800

    add dialect

---
+++ dialect/dialect.go
@@ -0,0 +1,18 @@
+package dialect
+
+import "reflect"
+
+var dialectsMap = make(map[string]Dialect)
+
+type Dialect interface {
+	DataTypeOf(typ reflect.Value) string                    // 将Go语言的类型转为该数据库的数据类型
+	TableExistSql(tableName string) (string, []interface{}) // 返回某个表是否存在的SQL语句，参数是表名(TABLE)
+}
+
+func RegisterDialect(name string, dialect Dialect) {
+	dialectsMap[name] = dialect
+}
+
+func GetDialect(name string) Dialect {
+	return dialectsMap[name]
+}
