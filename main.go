6b45138e5cfc5c15241ad9955846da761d3f1522
yuxintao <yuxintao.jingtao8a@bytedance.com>
2025-04-21 20:07:07 +0800

    测试Engine

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"gee-orm/log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Hello struct {
+	Name string `geeorm:"PRIMARY KEY"`
+	Age  int
+}
+
+func main() {
+	engine, err := NewEngine("sqlite3", "/Users/bytedance/gee-orm/gee.db")
+	if err != nil {
+		log.Error(err)
+	}
+	session := engine.NewSession().Model(&Hello{})
+	if session.HasTable() {
+		session.DropTable()
+	}
+	session.CreateTable()
+	res, err := session.Raw("INSERT INTO Hello(`Name`, `Age`) values (?, ?), (?,?)", "Tom", 12, "Sam", 13).Exec()
+	if res == nil || err != nil {
+		log.Error("INSERT FAILED1")
+	}
+	i, err := res.RowsAffected()
+	if err != nil || i != 2 {
+		log.Error("INSERT FAILED2")
+	}
+}
