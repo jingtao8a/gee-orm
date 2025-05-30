ff25d4521b98ba1773638d63d438e59651b6b9a6
jingtao8a <1921056015@qq.com>
2025-05-30 17:51:56 +0800

    feat: record.go add Update/Delete/Count/Limit/Where/OrderBy/First functions

--- session/record.go
+++ session/record.go
@@ -1,6 +1,7 @@
 package session
 
 import (
+	"errors"
 	"gee-orm/clause"
 	"reflect"
 )
@@ -69,6 +70,17 @@ func (s *Session) Update(kv ...interface{}) (int64, error) {
 	return result.RowsAffected()
 }
 
+// Delete records with where clause
+func (s *Session) Delete() (int64, error) {
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
 // Count records with where clause
 func (s *Session) Count() (int64, error) {
 	s.clause.Set(clause.COUNT, s.RefTable().Name)
@@ -79,4 +91,36 @@ func (s *Session) Count() (int64, error) {
 		return 0, err
 	}
 	return tmp, nil
+}
+
+// Limit adds limit condition to clause
+func (s *Session) Limit(num int) *Session {
+	s.clause.Set(clause.LIMIT, num)
+	return s
+}
+
+// Where adds limit condition to clause
+func (s *Session) Where(desc string, args ...interface{}) *Session {
+	var vars []interface{}
+	s.clause.Set(clause.WHERE, append(append(vars, desc), args...)...)
+	return s
+}
+
+// OrderBy adds order by condition to clause
+func (s *Session) OrderBy(desc string) *Session {
+	s.clause.Set(clause.ORDERBY, desc)
+	return s
+}
+
+func (s *Session) First(value interface{}) error {
+	dest := reflect.Indirect(reflect.ValueOf(value))
+	destSlice := reflect.New(reflect.SliceOf(dest.Type())).Elem()
+	if err := s.Limit(1).Find(destSlice.Addr().Interface()); err != nil {
+		return err
+	}
+	if destSlice.Len() == 0 {
+		return errors.New("NOT FOUND")
+	}
+	dest.Set(destSlice.Index(0))
+	return nil
 }
